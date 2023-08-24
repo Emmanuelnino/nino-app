610aea3b0c5db95308fb9fa336fbc0acec986a6b
emmanuelnino <emmanuel98tembo@gmail.com>
2023-08-24 15:21:36 -0700

    change

--- src/pages/CheckResults.jsx
+++ src/pages/CheckResults.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const CheckResults = () => {
   return (
-    <div>CheckResults</div>
+    <div className='flex min-h-screen justify-center items-center w-full'>
+      <div>cjhuigy</div>
+
+    </div>
   )
 }
 
