5ce0ad77d1e115a64b032dfa682e911cb650fd69
emmanuelnino <emmanuel98tembo@gmail.com>
2023-08-24 11:26:13 -0700

    change

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -14,25 +14,7 @@ const Nav = () => {
                     <span className="md:flex hidden">Stone</span>
                     <span className="md:flex hidden text-green-400">WreckOdds</span>
                 </a>
-                <button
-                    onClick={toggleNav}
-                    className="show-nav md:hidden text-3xl cursor-pointer"
-                >
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke-width="1.5"
-                        stroke="currentColor"
-                        className="w-6 h-6"
-                    >
-                        <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-                        />
-                    </svg>
-                </button>
+
 
                 {/* Mobile Nav */}
                 <nav
